f1f5daa33235ff82057b6ab595a78b82fa4d0a0f
Oguzcan Aygun <aygunoguzcan@outlook.com>
2023-06-04 18:37:27 +0300

    scnd

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(express.urlencoded({
+  extended:true
+}))
+app.use(express.json());
+
+app.get("/", (req, res) => {
+res.send("hello world")
+})
+
+app.get("/", (req, res) => {
+
+})
+
+app.listen(PORT, () => {
+  console.log('Server started on port ${PORT}');
+})
